2e6ee778dc14759ec737a9079b07bc442f764a1b
David Tejuosho <tejuoshodavid@gmail.com>
2022-02-09 07:14:17 -0600

    Changed footer pushing to be handled by JavaScript

--- themeSwapper.js
+++ themeSwapper.js
@@ -11,7 +11,23 @@ function onPageLoad() {
 	} else {
 		pageTheme.setAttribute("href", "/styling/dark.css");
 	}
+	console.log("I made it");
+	pushFooter();
 }
+
+function pushFooter() {
+	const footer = document.getElementById("footer");
+	const main = document.getElementById("main");
+	const header = document.getElementById("header");
+
+	let headerHeight = getComputedStyle(header).height;
+	let footerHeight = getComputedStyle(footer).height;
+
+	main.style.minHeight =
+		"calc(" + "100vh - 96px - " + headerHeight + " - " + footerHeight + ")";
+	//-96px to account for margins
+}
+
 function swapThemeStyleSheet() {
 	const pageTheme = document.getElementById("page-color");
 	if (sessionStorage.getItem("isDark") == "false") {
