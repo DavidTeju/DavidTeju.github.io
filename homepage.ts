cdeab524ef8dc6255d9b67b06b8207e5a7dce1b3
David Tejuosho <tejuoshodavid@gmail.com>
2023-02-22 14:49:30 -0600

    added scroll to top and fixed dark mode box shadow

--- homepage.ts
+++ homepage.ts
@@ -1,10 +1,14 @@
 function stretchMain() {
-	const main = document.querySelector("#main") as HTMLElement;
-	let toStretch = document.querySelector("#front") as HTMLElement;
+    const main = document.querySelector("#main") as HTMLElement;
+    let toStretch = document.querySelector("#front") as HTMLElement;
 
-	toStretch.style.minHeight = `calc(100vh - ${
-		getComputedStyle(main).marginTop
-	} - ${getComputedStyle(toStretch).marginBottom})`;
+    toStretch.style.minHeight = `calc(100vh - ${
+        getComputedStyle(main).marginTop
+    } - ${getComputedStyle(toStretch).marginBottom})`;
+}
+
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: "smooth"});
 }
 
 function onScroll() {
@@ -16,7 +20,6 @@ function onScroll() {
         .map(((sum = 0), (n) => (sum += n)))
         .map((mem) => mem - window.innerHeight / 3);
 
-    let highlightShadow = "0 0 5px 5px #1b2027";
     let focused = 0;
 
     for (let i = 0; i < cumSizes.length; i++) {
@@ -25,13 +28,29 @@ function onScroll() {
             break;
         }
     }
-    sections.forEach((section) => (section.style.boxShadow = ""));
 
-    sections[focused].style.boxShadow = highlightShadow;
+    changeScrollButton(focused == 0);
+
+    sections.forEach((section) => section.classList.remove("selected"));
+
+    sections[focused].classList.add("selected");
 }
 
+function changeScrollButton(shouldEnable: boolean) {
+    if (shouldEnable) {
+        scrollButton.style.opacity = "0";
+        scrollButton.setAttribute("disabled", "");
+    } else {
+        scrollButton.style.opacity = "1";
+        if (scrollButton.attributes.getNamedItem("disabled"))
+            scrollButton.attributes.removeNamedItem("disabled");
+    }
+}
 
 stretchMain();
+
+const scrollButton: HTMLElement = document.querySelector("#scroll-to-top")!;
+scrollButton.addEventListener("click", scrollToTop);
 
 let sections = Array.from(
     document.querySelectorAll("main>section").values()
